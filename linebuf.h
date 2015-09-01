8f8174f47c1ef934ed0b8fa7d9e7b6f50413e91a
AppleDash <derp@derpy.fr>
2015-09-01 05:10:00 -0400

    Basic documentation.

--- linebuf.h
+++ linebuf.h
@@ -9,8 +9,24 @@ typedef struct linebuffer {
 	int maxlen;
 } linebuf_t;
 
+/**
+ * Create a new line buffer
+ */
 linebuf_t *linebuf_new();
+
+/**
+ * Destroy a line buffer (free it and its contents from memory)
+ */
 void linebuf_destroy(linebuf_t *linebuf);
 
+/**
+ * Append arbitrary data to a line buffer
+ * This data may contain anywhere from less than a single full line
+ * to multiple lines ending in another partial line.
+ */
 void linebuf_append(linebuf_t *linebuf, char *data, int data_len);
+
+/**
+ * Pop a line from a line buffer, if it has one or more.
+ */
 bool linebuf_pop(linebuf_t *linebuf, char **outline);
